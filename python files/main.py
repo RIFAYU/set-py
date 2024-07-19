20d0a548cd86a89a060f1f6cef96f672b73119e2
Rifayudeen Q <q.rifayudeen@gmail.com>
2024-07-19 15:35:16 +0530

    corona data scrap

--- python files/main.py
+++ python files/main.py
@@ -1,2 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+
+url = "https://www.worldometers.info/coronavirus/"
+
+response = requests.get(url)
+
+soup = BeautifulSoup(response.text, "html.parser")
+
+table = soup.find("table", id="main_table_countries_today")
+
+rows = table.find_all("tr")
+
+for row in rows:
+    cols = row.find_all("td")
+    cols = [ele.text.strip() for ele in cols]
+    print(*cols)
